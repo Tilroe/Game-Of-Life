b407bb153e9f21f34bc75df794e6e556d00a9698
Tilroe <rowan@tilroe.com>
2024-09-08 15:26:49 -0600

    update method for sparse matrix

--- game/SparseMatrix.cpp
+++ game/SparseMatrix.cpp
@@ -13,8 +13,51 @@ SparseMatrix::SparseMatrix(const int width, const int height) :
 
 void SparseMatrix::update()
 {
-	
-	return;
+	next_->clear();
+	std::unordered_map<Point, int> neighbours;
+
+	// Calculate neighbour counts
+	for (const Point& p : *current_) {
+		for (int x_offset : {-1, 0, 1}) {
+			for (int y_offset : {-1, 0, 1}) {
+				Point neighbour{ p.x + x_offset, p.y + y_offset };
+
+				// Don't count neighbour for self
+				if (neighbour == p) continue;
+
+				// Bounds check
+				if ((neighbour.x < 0) ||
+					(neighbour.x >= width) ||
+					(neighbour.y < 0) ||
+					(neighbour.y >= height)) continue;
+
+				auto result = neighbours.emplace(neighbour, 1);
+				bool exists = result.second;
+				std::unordered_map<Point, int>::iterator iter = result.first;
+				if (!exists) {
+					// neighbour already exists in neighbours, just update count
+					iter->second = iter->second + 1;
+				}
+			}
+		}
+	}
+
+	// Figure out which points need to be alive
+	for (const std::pair<Point, int>& kv : neighbours) {
+		const Point& p = kv.first;
+		const int& n_neighbours = kv.second;
+
+		if (current_->find(p) == current_->end()) {
+			// Point is not alive
+			if (n_neighbours == 3) { next_->emplace(p); }
+		}
+		else {
+			// Point is alive
+			if (n_neighbours == 2 || n_neighbours == 3) { next_->emplace(p); }
+		}
+	}
+
+	swap();
 }
 
 void SparseMatrix::draw() const
@@ -24,5 +67,7 @@ void SparseMatrix::draw() const
 
 void SparseMatrix::swap()
 {
-	return; // TODO
+	std::unordered_set<Point>* temp = current_;
+	current_ = next_;
+	next_ = temp;
 }
