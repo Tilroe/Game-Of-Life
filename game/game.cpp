0ceb3744a08eab680aea213191d4689f3cfa2252
Tilroe <rowan@tilroe.com>
2024-09-12 13:29:22 -0600

    zoom & movement clamping

--- game/game.cpp
+++ game/game.cpp
@@ -9,6 +9,7 @@ Game::Game(const int width, const int height) :
 	width(width),
 	height(height)
 {
+
 	kernels.push_back(new SparseMatrix(width, height));
 	current_kernel = kernels[0];
 }
@@ -22,13 +23,7 @@ Game::~Game()
 
 void Game::update(float dt)
 {
-	// Camera updates
-	if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
-		Vector2 mouse_delta = Vector2Scale(GetMouseDelta(), -1);
-		camera.target = Vector2Add(camera.target, mouse_delta);
-		camera.target = Vector2Clamp(camera.target, { 0, 0 }, { width * 32 - (float)GetScreenWidth(), height * 32 - (float)GetScreenHeight() });
-	}
-
+	handle_camera();
 
 	// Kernel updates
 	if (state == PLAY) {
@@ -41,9 +36,61 @@ void Game::update(float dt)
 	
 }
 
+// Camera helper function forward declarations
+void handle_movement(Camera2D& camera); 
+void target_on_mouse(Camera2D& camera);  
+void handle_zoom(Camera2D& camera);      
+void camera_bounds_correction(Camera2D& camera, Vector2 bounds);  
+
+void Game::handle_camera()
+{
+	target_on_mouse(camera);
+	handle_movement(camera);
+	handle_zoom(camera);
+	camera_bounds_correction(camera, get_bounds());
+}
+
+// --- Camera helper functions ---
+static void handle_movement(Camera2D& camera)
+{
+	if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
+		Vector2 mouse_delta = Vector2Scale(GetMouseDelta(), -1 / camera.zoom);
+		camera.target = Vector2Add(camera.target, mouse_delta);
+	}
+}
+
+static void target_on_mouse(Camera2D& camera)
+{
+	Vector2 mouse_world_pos = GetScreenToWorld2D(GetMousePosition(), camera);
+	camera.offset = GetMousePosition();
+	camera.target = mouse_world_pos;
+}
+
+static void handle_zoom(Camera2D& camera)
+{
+	float wheel = GetMouseWheelMove();
+	if (wheel) {
+		float scale_factor = 1.0f + (0.25f * fabsf(wheel));
+		if (wheel < 0) scale_factor = 1.0f / scale_factor;
+		camera.zoom = Clamp(camera.zoom * scale_factor, 0.125f, 64.0f);
+	}
+}
+
+static void camera_bounds_correction(Camera2D& camera, Vector2 bounds)
+{
+	camera.target = Vector2Clamp(camera.target, Vector2{}, bounds);
+}
+// ------------------------------
+
 void Game::draw() const
 {
+	Vector2 bounds = get_bounds();
 	BeginMode2D(camera);
+	DrawLineEx(Vector2{ 0, 0 }, Vector2{ bounds.x }, 20.f, GRAY);
+	DrawLineEx(Vector2{ bounds.x, 0 }, Vector2{ bounds.x, bounds.y }, 20.f, GRAY);
+	DrawLineEx(bounds, Vector2{ 0, bounds.y }, 10.f, GRAY);
+	DrawLineEx(Vector2{ 0, bounds.y }, Vector2{ 0, 0 }, 20.f, GRAY);
+	DrawCircle((int)camera.target.x, (int)camera.target.y, 20.f, RED);
 	current_kernel->draw();
 	EndMode2D();
 }
--- game/game.h
+++ game/game.h
@@ -13,15 +13,23 @@ public:
 
 	enum GameState { PAUSE, PLAY };
 	inline GameState& get_game_state() { return state; }
+	inline Vector2 get_bounds() const { return Vector2{ (float)width*TILE_SIZE, (float)height*TILE_SIZE }; }
 
 private:
+	GameState state = PLAY;
+
+	// Camera members
+	void handle_camera();
 	Camera2D camera{ {0.f, 0.f}, {0.f, 0.f}, 0.f, 1.f };
 
+	// Grid members
+	const int TILE_SIZE = 32;
+	const int width, height;
+
+	// Kernel members
 	Kernel* current_kernel;
 	std::vector<Kernel*> kernels;
-	GameState state = PLAY;
-	const int width, height;
 	float time = 0.f;
 	const float update_time = 0.5f;
-	
+
 };
